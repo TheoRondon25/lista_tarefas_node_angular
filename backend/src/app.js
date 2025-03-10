57550a4cd77a72c6123f6c8067f12dd8edb0481d
Theo Rondon <theoprondon@hotmail.com>
2025-03-10 16:51:24 -0300

    fazendo e aprendendo Angular, testando componentes, entendendo melhor sobre a estrutura padrao

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: 'http://localhost:4200', // ALTERADO AQUI PARA QUE EU POSSA ACESSAR A API PELO FRONT - APAGAR DEPOIS -
+    methods: 'GET,POST,PUT,DELETE',
+    allowefHeaders: 'Content-Type, Authorization'
+}));
 
 app.use('/lista_tarefas', tarefaRoutes)
 
---
+++ frontend/src/app/components/task/task.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+
+@Component({
+  selector: 'app-task',
+  standalone: false,
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent implements OnInit {
+  tasks: any[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
+    this.taskService.getTasks().subscribe((data) => {
+      this.tasks = data;
+    });
+  }
+}
